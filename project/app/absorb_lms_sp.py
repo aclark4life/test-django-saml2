8f6fd246926eaa011fdbe54facbabd6840517164
Alex Clark <aclark@aclark.net>
2017-05-14 09:48:00 -0400

    Update

--- project/app/absorb_lms_sp.py
+++ project/app/absorb_lms_sp.py
@@ -9,8 +9,23 @@ class Processor(idptest.saml2idp.base.Processor):
         self._assertion_xml = idptest.saml2idp.xml_render.get_assertion_absorblms_xml(self._assertion_params, signed=True)
 
     def can_handle(self, request):
-        return True
+        """
+        Returns true if this processor can handle this request. XXX Don't need to override this method, just need a convenient place to debug.
+        """
+        self._reset(request)
+        # Read the request.
+        import pdb ; pdb.set_trace()
+        try:
+            self._extract_saml_request()
+            self._decode_request()
+            self._parse_request()
+        except Exception, e:
+            msg = 'Exception while reading request: %s' % e
+            self._logger.debug(msg)
+            raise exceptions.CannotHandleAssertion(msg)
 
+        self._validate_request()
+        return True
 
 class AttributeProcessor(idptest.saml2idp.base.Processor):
     """
