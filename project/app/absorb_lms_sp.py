273343421c5250f81857fd0728dd64cf3a49d92b
Alex Clark <aclark@aclark.net>
2017-05-14 09:28:09 -0400

    Update

--- project/app/absorb_lms_sp.py
+++ project/app/absorb_lms_sp.py
@@ -1,12 +1,22 @@
-# Absorb LMS Service Provider Processor
-
-from idptest.saml2idp import base
+import base
+import exceptions
+import xml_render
 
 class Processor(base.Processor):
     """
+    Absorb LMS Response Handler Processor for testing against django-saml2-sp.
     """
+    def _format_assertion(self):
+        self._assertion_xml = xml_render.get_assertion_absorb_xml(self._assertion_params, signed=True)
 
-    def can_handle(self, request):
-        """
-        """
-        return True
+
+class AttributeProcessor(base.Processor):
+    """
+    Absorb LMS Response Handler Processor for testing against django-saml2-sp;
+    Adds SAML attributes to the assertion.
+    """
+    def _format_assertion(self):
+        self._assertion_params['ATTRIBUTES'] = {
+            'foo': 'bar',
+        }
+        self._assertion_xml = xml_render.get_assertion_absorb_xml(self._assertion_params, signed=True)
