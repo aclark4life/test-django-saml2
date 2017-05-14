fff3cdf40fa2521c37f62e31b48f939a23a5a021
Alex Clark <aclark@aclark.net>
2017-05-14 09:36:33 -0400

    Update

--- project/app/absorb_lms_sp.py
+++ project/app/absorb_lms_sp.py
@@ -1,15 +1,15 @@
 import idptest.saml2idp.base
 import idptest.saml2idp.xml_render
 
-class Processor(idptest.saml2idp.Processor):
+class Processor(idptest.saml2idp.base.Processor):
     """
     Absorb LMS Response Handler Processor for testing against django-saml2-sp.
     """
     def _format_assertion(self):
-        self._assertion_xml = idptest.saml2idp.get_assertion_absorblms_xml(self._assertion_params, signed=True)
+        self._assertion_xml = idptest.saml2idp.xml_render.get_assertion_absorblms_xml(self._assertion_params, signed=True)
 
 
-class AttributeProcessor(idptest.saml2idp.Processor):
+class AttributeProcessor(idptest.saml2idp.base.Processor):
     """
     Absorb LMS Response Handler Processor for testing against django-saml2-sp;
     Adds SAML attributes to the assertion.
@@ -18,4 +18,4 @@ class AttributeProcessor(idptest.saml2idp.Processor):
         self._assertion_params['ATTRIBUTES'] = {
             'foo': 'bar',
         }
-        self._assertion_xml = idptest.saml2idp.get_assertion_absorblms_xml(self._assertion_params, signed=True)
+        self._assertion_xml = idptest.saml2idp.xml_render.get_assertion_absorblms_xml(self._assertion_params, signed=True)
