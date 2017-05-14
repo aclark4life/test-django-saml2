e82b975299fe029c077a90e020c391814547a005
Alex Clark <aclark@aclark.net>
2017-05-14 09:10:10 -0400

    Update

--- project/urls.py
+++ project/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^idp/', include('saml2idp.urls')),
+    url(r'^idp/', include('idptest.saml2idp.urls')),
 ]
