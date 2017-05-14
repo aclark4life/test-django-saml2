335c828b0f80131751d252652df4a733f7a3856f
Alex Clark <aclark@aclark.net>
2017-05-13 20:46:51 -0400

    Update

--- project/app/models.py
+++ project/app/models.py
@@ -6,7 +6,7 @@ from django.db import models
 # http://stackoverflow.com/a/43209322/185820
 
 from django.contrib.auth.models import AbstractUser
-class User(AbstractUser):
+class MyUser(AbstractUser):
     USERNAME_FIELD = 'email'
     email = models.EmailField('email address', unique=True) # changes email to unique and blank to false
     REQUIRED_FIELDS = [] # removes email from REQUIRED_FIELDS
