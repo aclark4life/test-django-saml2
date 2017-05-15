d64c8627dc8d696ac440da1cc88a54ac48b0f6cd
Alex Clark <aclark@aclark.net>
2017-05-14 21:41:12 -0400

    Update

--- project/app/absorb_lms_sp.py
+++ project/app/absorb_lms_sp.py
@@ -1,9 +1,9 @@
-# from onelogin.saml2 import utils
+from onelogin.saml2 import utils
 import idptest.saml2idp.base
 import idptest.saml2idp.exceptions
 import idptest.saml2idp.xml_render
 
-# saml2_utils = utils.OneLogin_Saml2_Utils()
+saml2_utils = utils.OneLogin_Saml2_Utils()
 
 class Processor(idptest.saml2idp.base.Processor):
     """
@@ -34,23 +34,7 @@ class Processor(idptest.saml2idp.base.Processor):
         """
         Decodes _request_xml from _saml_request. Override base to use python-saml utils from OneLogin.
         """
-        # self._request_xml = saml2_utils.decode_base64_and_inflate(self._saml_request)
-        self._request_xml = """
-<samlp:AuthnRequest ID="_802c81b9-f6eb-47d5-adf3-4a8f3b12d11f"
-                    Version="2.0"
-                    IssueInstant="2017-05-14T17:12:43.757Z"
-                    ForceAuthn="false"
-                    IsPassive="false"
-                    ProtocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"
-                    AssertionConsumerServiceURL="https://aclark.myabsorb.com/account/saml"
-                    xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol"
-                    >
-    <saml:Issuer xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion">https://aclark.myabsorb.com</saml:Issuer>
-    <samlp:NameIDPolicy SPNameQualifier="Id"
-                        AllowCreate="true"
-                        />
-</samlp:AuthnRequest>
-"""
+        self._request_xml = saml2_utils.decode_base64_and_inflate(self._saml_request)
 
 class AttributeProcessor(idptest.saml2idp.base.Processor):
     """
