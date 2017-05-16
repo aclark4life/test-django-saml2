6a0e8da9bf8edc9ae8228697c6b65e876340391a
Alex Clark <aclark@aclark.net>
2017-05-15 22:34:51 -0400

    Update

--- project/settings.py
+++ project/settings.py
@@ -179,9 +179,9 @@ LOGIN_URL='/admin'
 
 SAML2IDP_CONFIG = {
     'autosubmit': False,
-    'certificate_file': os.path.join(BASE_DIR, 'project', 'keys', 'sample-certificate.pem'),
+    'certificate_file': os.path.join(BASE_DIR, 'project', 'keys', 'certificate.pem'),
     'issuer': 'https://dj-saml-idp.aclark.net',
-    'private_key_file': os.path.join(BASE_DIR, 'project', 'keys', 'sample-private-key.pem'),
+    'private_key_file': os.path.join(BASE_DIR, 'project', 'keys', 'private-key.pem'),
     'signing': True,
 }
 
