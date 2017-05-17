6f71ebd9862632415dbe17489aabe54ba4fd9c49
Alex Clark <aclark@aclark.net>
2017-05-17 17:32:35 -0400

    Update

--- project/app/views.py
+++ project/app/views.py
@@ -6,7 +6,6 @@ from idptest.saml2idp.views import _generate_response
 from project.app.absorb_lms_sp import Processor
 
 processor = Processor()
-processor._request_params = {}
 
 def generate_response(request):
     processor._reset(request)
