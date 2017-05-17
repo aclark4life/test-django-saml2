b6e558837e92a48c4f2469d58b7e4bdaf83114f1
Alex Clark <aclark@aclark.net>
2017-05-17 16:57:38 -0400

    Update

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from idptest.saml2idp.views import _generate_response
+from project.app.absorb_lms_sp import Processor
+
+processor = Processor()
+
+def generate_response(request):
+    return _generate_response(request, processor)
+    
--- project/urls.py
+++ project/urls.py
@@ -20,4 +20,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^idp/', include('idptest.saml2idp.urls')),
+    url(r'^generate-response$', 'project.app.views.generate_response'),
 ]
